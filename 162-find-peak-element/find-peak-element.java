5bf73096b8e0ddfed1f4acd03a76ad584466601c
Abhishek Bhaskar Kadam <abhishekkadam1061@gmail.com>
2023-11-29 12:50:07 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (44.44%) - LeetSync

--- 162-find-peak-element/find-peak-element.java
+++ 162-find-peak-element/find-peak-element.java
@@ -1,21 +1,25 @@
 class Solution {
     public int findPeakElement(int[] arr) {
-        int start =0;
-        int end  = arr.length-1, mid;
+        int start = 0;
+        int end = arr.length -1;
 
-        while(start <= end){
-        
-            mid  = start + (end - start) / 2;
-
-            if((mid == 0 || arr[mid] > arr[mid-1]) && (mid == arr.length -1 || arr[mid] > arr[mid +1])){
-                return mid;
-            }else if(mid > 0  && arr[mid -1] > arr[mid]){
-                end = mid -1;
-            }else{
-                start = mid + 1;
+        while(start < end){
+            int mid = start + (end-start)/2;
+            if(arr[mid]> arr[mid+1]){
+                //you are in the dec part of array
+                // this may be the answer but look at left
+                //this is why end != mid -1
+                end = mid;
+            }else {
+                //you are in asc part of array
+                start =mid +1; // because we know that mid +1 element > mid element
             }
-
         }
-        return -1;
+        //in the end, start == end and pointer to the largest number because of 2 checks above.
+        // start and end are always  trying to find max element, in the above 2 checks
+        // hence, when they are pointing to just one element, that is the max one because that is what check say
+        //more elaboration: at every point of time for start and end, they have the best possible answer till that time
+        //and if we are saying that one item is remaining, hence cuz of above line that is the bes possible ans
+        return start;
     }
 }
