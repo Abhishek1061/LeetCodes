111075a68a5bf1ebc6c6e093a655b21ee7fbcf84
Abhishek Bhaskar Kadam <abhishekkadam1061@gmail.com>
2023-12-11 08:18:37 +0530

    Time: 5 ms (59.18%) | Memory: 56.5 MB (57.90%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/find-the-duplicate-number.java
@@ -2,23 +2,39 @@ class Solution {
     public int findDuplicate(int[] nums) {
 
         // My logic
-         int i = 0;
-          while(i < nums.length){
-              int correct = nums[i] -1;
-              if(nums[i] != nums[correct]){
-                  swap(nums, i, correct);
-              }else{
-                  i++;
-              }
-          }
-          //Find the duplicates
-        int ans = 0;
-        for (int index = 0; index < nums.length; index++) {
-            if(nums[index] != index +1){
-                ans = nums[index];
-            }
+        //  int i = 0;
+        //   while(i < nums.length){
+        //       int correct = nums[i] -1;
+        //       if(nums[i] != nums[correct]){
+        //           swap(nums, i, correct);
+        //       }else{
+        //           i++;
+        //       }
+        //   }
+        //   //Find the duplicates
+        // int ans = 0;
+        // for (int index = 0; index < nums.length; index++) {
+        //     if(nums[index] != index +1){
+        //         ans = nums[index];
+        //     }
 
-        }return ans;
+        // }return ans;
+
+
+        //2nd logic
+        int i = 0;
+        while(i < nums.length){
+            if(nums[i] != i + 1){
+                int correct = nums[i] - 1;
+                if(nums[i] != nums[correct]){
+                    swap(nums, i, correct);
+                }else{
+                    return nums[i];
+                }
+            }else {
+                i++;
+            }
+        }return -1;
     }
     static void swap(int[] arr, int first, int second){
         int temp = arr[first];
